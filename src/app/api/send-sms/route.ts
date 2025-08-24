4580e03436acd14a26da69370e28a53137d0cca0
Abdulloev Buzurgmehr <thesarboz@gmail.com>
2025-08-25 01:52:48 +0500

    design changes 1

--- src/app/api/send-sms/route.ts
+++ src/app/api/send-sms/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: NextRequest) {
     }
 
     let smsCount = await prisma.sendSMS.count()
-    let txn_id: number = 1450 + smsCount
+    let txn_id: number = 1500 + smsCount
     await prisma.sendSMS.create({
       data: {
         phoneNumber: phoneNumber,
