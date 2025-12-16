b8f191b03d18502b8bb13bc4fb6fca3a51ca7512
Abdulloev Buzurgmehr <thesarboz@gmail.com>
2025-12-16 17:02:38 +0500

    changes to sync code

--- src/app/api/sync-with-one-c/route.ts
+++ src/app/api/sync-with-one-c/route.ts
@@ -1,11 +1,12 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
 import { AdditionalContactInformation, Candidate1CPayload, EducationItem, formatDateTo1C, LanguageKnowledge, sendTo1C, WorkExperienceItem } from "../1c";
 
 export async function GET(request: NextRequest) {
 
+  let seekers;
   try {
-    const seekers = await prisma.jobSeeker.findMany({
+    seekers = await prisma.jobSeeker.findMany({
       where: {
         syncedWith1C: false,
       },
@@ -16,53 +17,58 @@ export async function GET(request: NextRequest) {
         knowledgeOfLanguages: true
       }
     })
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ status: "Провал" }, { status: 500 })
+  }
 
-    for (const seeker of seekers) {
-      const profileFor1C: Candidate1CPayload = {
-        lastName: seeker.lastName,
-        firstName: seeker.firstName,
-        middleName: seeker.middleName,
-        birthDate: formatDateTo1C(seeker.birthDate.toDateString()),
-        tin: seeker.tin,
-        gender: seeker.gender,
-        email: seeker.email,
-        maritalStatus: seeker.maritalStatus,
-        passportCode: seeker.passportCode,
-        phoneNumber: seeker.phoneNumber,
-        messengerNumber: seeker.messengerNumber,
-        address: seeker.address,
-        addressOfBirth: seeker.addressOfBirth,
-        desiredSalary: seeker.desiredSalary,
-        dateOfReadiness: formatDateTo1C(seeker.dateOfReadiness.toDateString()),
-        desiredCountry: seeker.desiredCountry,
-        desiredCity: seeker.desiredCity,
-        criminalRecord: seeker.criminalRecord,
-        additionalInformation: seeker.additionalInformation ?? "",
-        createdAt: formatDateTo1C(seeker.createdAt.toDateString()),
-        updatedAt: formatDateTo1C(seeker.updatedAt.toDateString()),
-        additionalContactInformation: seeker.additionalContactInformation.map<AdditionalContactInformation>((val) => ({
-          fullname: val.fullname,
-          phone_number: val.phoneNumber,
-          status: val.status,
-        })),
-        WorkExperience: seeker.WorkExperience.map<WorkExperienceItem>((val) => ({
-          company: val.workplace,
-          position: val.jobTitle,
-          startDate: formatDateTo1C(val.dateStart.toDateString()),
-          endDate: formatDateTo1C(val.dateEnd?.toDateString() ?? "")
-        })),
-        education: seeker.education.map<EducationItem>(val => ({
-          education: val.education,
-          institution: val.institution,
-          specialty: val.specialty,
-        })),
-        knowledgeOfLanguages: seeker.knowledgeOfLanguages.map<LanguageKnowledge>(val => ({
-          language: val.language,
-          level: val.level
-        }))
-      }
+  for (const seeker of seekers) {
+    const profileFor1C: Candidate1CPayload = {
+      lastName: seeker.lastName,
+      firstName: seeker.firstName,
+      middleName: seeker.middleName,
+      birthDate: formatDateTo1C(seeker.birthDate.toDateString()),
+      tin: seeker.tin,
+      gender: seeker.gender,
+      email: seeker.email,
+      maritalStatus: seeker.maritalStatus,
+      passportCode: seeker.passportCode,
+      phoneNumber: seeker.phoneNumber,
+      messengerNumber: seeker.messengerNumber,
+      address: seeker.address,
+      addressOfBirth: seeker.addressOfBirth,
+      desiredSalary: seeker.desiredSalary,
+      dateOfReadiness: formatDateTo1C(seeker.dateOfReadiness.toDateString()),
+      desiredCountry: seeker.desiredCountry,
+      desiredCity: seeker.desiredCity,
+      criminalRecord: seeker.criminalRecord,
+      additionalInformation: seeker.additionalInformation ?? "",
+      createdAt: formatDateTo1C(seeker.createdAt.toDateString()),
+      updatedAt: formatDateTo1C(seeker.updatedAt.toDateString()),
+      additionalContactInformation: seeker.additionalContactInformation.map<AdditionalContactInformation>((val) => ({
+        fullname: val.fullname,
+        phone_number: val.phoneNumber,
+        status: val.status,
+      })),
+      WorkExperience: seeker.WorkExperience.map<WorkExperienceItem>((val) => ({
+        company: val.workplace,
+        position: val.jobTitle,
+        startDate: formatDateTo1C(val.dateStart.toDateString()),
+        endDate: formatDateTo1C(val.dateEnd?.toDateString() ?? "")
+      })),
+      education: seeker.education.map<EducationItem>(val => ({
+        education: val.education,
+        institution: val.institution,
+        specialty: val.specialty,
+      })),
+      knowledgeOfLanguages: seeker.knowledgeOfLanguages.map<LanguageKnowledge>(val => ({
+        language: val.language,
+        level: val.level
+      }))
+    }
 
-      if (await sendTo1C(profileFor1C)) {
+    if (await sendTo1C(profileFor1C)) {
+      try {
         await prisma.jobSeeker.updateMany({
           where: {
             id: seeker.id,
@@ -71,10 +77,10 @@ export async function GET(request: NextRequest) {
             syncedWith1C: true,
           }
         })
+      } catch (err) {
+        console.log(err)
       }
     }
-
-  } catch {
-    return
   }
+
 }
